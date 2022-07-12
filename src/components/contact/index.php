c4ee010d42a371b293faddbf6c04262a93c899b1
José Cuevas NL <naver.developer@gmail.com>
2022-07-12 12:03:14 -0500

    footer

--- src/components/contact/index.php
+++ src/components/contact/index.php
@@ -1,31 +1,12 @@
-<div id="rec442645419" class="r t-rec r_hidden r_anim" style=" " data-record-type="215"><a name="contacts"
-        style="font-size:0;"></a></div>
-<div id="rec442612395" class="r t-rec" style=" " data-animationappear="off" data-record-type="570">
-    <!-- t570 -->
-    <div class="t570">
-        <div class="t-width t-width_100 t570__mainblock" style="margin:0 auto; background-color:#0000ff;">
-            <div id="frame" class="t570__col ">
-                <iframe id="JotFormIFrame-221886418012050" title="Contacto"
-                    src="https://form.jotform.com/221886418012050" frameborder="0"
-                    style=" min-width: 100%; min-height: 100%; border:none;" scrolling="yes"> </iframe>
+<div class="t-tildalabel t-tildalabel-free" id="tildacopy" data-tilda-sign="5540153#27409564"
+    style="display: block !important; visibility: visible !important; position: relative !important; width: 100% !important; pointer-events: all !important; opacity: 1 !important; margin: 0 !important; z-index: 1 !important">
+    <div class="t-tildalabel-free__main">
 
+        <?php
+        include "src/components/contact/sociallinks/index.php";
+        ?>
 
-            </div>
-            <div class="t570__col t570__col_text" style="background-color:#0000ff;">
-                <div class="t570__contentbox" style="background-color:#0000ff; height:80vh;">
-                    <div class="t570__contentwrapper t570__padding_lg">
-                        <div class="t570__title t-heading t-heading_sm" style="" field="title">Contáctenos:
-                        </div>
-
-                        <?php
-                                   
-                                   include "src/components/contact/sociallinks/index.php";
-                                   
-                                   ?>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
-
+    <div class="t-tildalabel-free__links-wr">
+    </div>
 </div>
